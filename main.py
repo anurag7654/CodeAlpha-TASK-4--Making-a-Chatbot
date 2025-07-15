7dce51db6a9967760d122398b60f53ab0031fa9f
root <root@vbox.hgu_lan>
2025-07-16 02:03:18 +0530

    Added dot env module in  AI Model  GroqAI

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from dotenv import load_dotenv
 import os
 from groq import Groq
 import traceback
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 
 client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
+    api_key=os.getenv("GROQ_API_KEY"),
 )
 
 @app.route("/")
